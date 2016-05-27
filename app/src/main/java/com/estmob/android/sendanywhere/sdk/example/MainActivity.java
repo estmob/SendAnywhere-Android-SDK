60d70f29658f4ac9b23e4001e426bd1eb3d3f7d0
Bumsoo Kim <bskim45@gmail.com>
2016-05-27 15:18:14 +0900

    replace depreciated ActionBarActivity

--- app/src/main/java/com/estmob/android/sendanywhere/sdk/example/MainActivity.java
+++ app/src/main/java/com/estmob/android/sendanywhere/sdk/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.estmob.android.sendanywhere.sdk.example;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
