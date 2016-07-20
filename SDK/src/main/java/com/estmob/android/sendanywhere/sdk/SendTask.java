479c2f7526ea2c8592bce64aa82e3d0b32c027db
Bumsoo Kim <bskim45@gmail.com>
2016-07-20 18:25:23 +0900

    added fileinfo interface

--- SDK/src/main/java/com/estmob/android/sendanywhere/sdk/SendTask.java
+++ SDK/src/main/java/com/estmob/android/sendanywhere/sdk/SendTask.java
@@ -1,17 +1,42 @@
 package com.estmob.android.sendanywhere.sdk;
 
 import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 
 import com.estmob.paprika.transfer.UploadTask;
 
 import java.io.File;
+import java.util.List;
 
 public class SendTask extends Task {
     public class DetailedState extends Task.DetailedState {
         public static final int ERROR_NO_REQUEST =  (State.ERROR << 8) + 20;
     }
 
+    public interface FileInfo extends UploadTask.FileInfo {
+        @NonNull
+        @Override
+        Uri getUri();
+
+        @NonNull
+        @Override
+        String getFileName();
+
+        @Override
+        long getLength();
+
+        @Override
+        long getLastModified();
+    }
+
     public SendTask(Context context, File[] files) {
+        super(context);
+
+        task = new UploadTask(context, files);
+    }
+
+    public SendTask(Context context, List<? extends FileInfo> files) {
         super(context);
 
         task = new UploadTask(context, files);
